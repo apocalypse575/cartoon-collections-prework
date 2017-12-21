36549d71bdcfce3d06d5b650121fc9d50a550b88
apocalypse575 <apocalypse575@gmail.com>
2017-12-21 01:57:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,62 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+  list = []
+  counter = 0
+  while counter < dwarves.length
+  list.push("#{counter + 1}. #{dwarves[counter]}")
+  counter += 1
+  end
+  puts list.join(" ")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  calls = []
+  counter = 0
+  while counter < planeteer_calls.length
+    calls.push("#{planeteer_calls[counter].capitalize}!")
+    counter += 1
+  end
+  return calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# short_words = ["puff", "go", "two"]
+# assorted_words = ["two", "go", "industrious", "bop"]
+# test = ["fears"]
+
+def long_planeteer_calls(words)
+  counter = 0
+  while counter < words.length
+  if words[counter].length > 4
+    return true
+  else
+    return false
+    counter += 1
+  end
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# long_planeteer_calls(assorted_words)
+
+# cheese_types = ["cheddar", "gouda", "camembert"]
+ingredients = ["garlic", "rosemary", "bread"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  counter = 0
+  until counter == cheese.length
+  return cheese.include?(cheese_types)
+    # return cheese[counter]
+    counter += 1
+  # end
+  end
 end
+
+
+
+# find_the_cheese(soup)
